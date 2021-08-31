a29cacc2f45fbc0017415b0c87c31e5c0e272371
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2021-08-31 17:11:16 +0900

    F is monotonic

--- ltl/eqnSys.v
+++ ltl/eqnSys.v
@@ -166,10 +166,16 @@ Qed.
 
 (* monotonicity *)
 
+Section Monotonicity.
+
+Definition env_leq (u1 u2 : Env) : Prop :=
+  forall v : V,
+  forall (theta : Theta) (w : data_word),
+  u1 v theta w -> u2 v theta w.
+
 Lemma models_is_monotonic :
   forall (u1 u2 : Env),
-  (forall (v : V) (theta : Theta) (w : data_word),
-   u1 v theta w -> u2 v theta w) ->
+  env_leq u1 u2 ->
   forall (psi : ltl) (theta : Theta) (w : data_word),
   (w, theta |= u1, psi) -> (w, theta |= u2, psi).
 Proof.
@@ -206,119 +212,55 @@ Proof.
   now inversion Hm.
 Qed.
 
-Theorem F_is_monotonic_1 :
+Theorem F_is_monotonic :
+  forall (sigma : eqn_sys) (u1 u2 : Env),
+  env_leq u1 u2 ->
+  env_leq (F sigma u1) (F sigma u2).
+Proof.
+  intros sigma u1 u2 H.
+  unfold env_leq.
+  unfold F.
+  intros v.
+  now apply models_is_monotonic.
+Qed.
+
+Theorem Fpow_is_monotonic_1 :
   forall (sigma : eqn_sys) (i : nat),
-  forall v theta w,
-  (Fpow_emp sigma i) v theta w -> Fpow_emp sigma (S i) v theta w.
+  env_leq (Fpow_emp sigma i) (Fpow_emp sigma (S i)).
 Proof.
   intros sigma i.
   induction i.
   { intros v theta w H.
     now unfold Fpow_emp in H. }
-  intros v.
   unfold Fpow_emp.
   unfold Fpow.
-  unfold F at 1.
-  unfold F at 2.
-  induction (sigma v); intros theta w H.
-  (* H: (w, theta |= Fpow sigma i, sigma v) *)
-  + (* sigma v = var v0 -> ... *)
-  apply models_var.
-  apply IHi.  (* apply (IHi v0). *)
-  now inversion H.
-  + (* sigma v = l1 .\/ l2 -> ... *)
-  apply models_OR.
-  inversion H as [| | | | w' th' u p1 p2 Ho| | |].
-  destruct Ho as [Ho | Ho].
-  * left; auto.
-  * right; auto.
-  + (* sigma v = l ./\ l0 -> ... *)
-  inversion H.
-  apply models_AND; auto.
-  + (* sigma v = ↓ r, l -> ... *)
-  inversion H as [| h t th u r' psi H' EQu EQht | | | | | |].
-  apply models_STORE; auto.
-  + (* sigma v = X l -> ... *)
-  inversion H.
-  apply models_X; auto.
-  + (* sigma v = l U l0 -> ... *)
-  inversion H
-  as [| | | | | | w' th' u phi psi' Hu EQu EQw'|].
-  clear w' EQw'.
-  destruct Hu as [w' [Hu' Hu]].
-  apply models_U.
-  exists w'.
-  split; auto.
-  + (* sigma v = φ l -> ... *)
-  inversion H.
-  now apply models_PHI.
-  + (* sigma v = G l -> ... *)
-  inversion H.
-  now apply models_G.
+  now apply F_is_monotonic.
 Qed.
 
-Theorem F_is_monotonic_2 :
+Theorem Fpow_is_monotonic_2 :
   forall (sigma : eqn_sys) (i : nat) (Y : Env),
-  (forall v theta w,
-   Fpow_emp sigma i v theta w -> Y v theta w) ->
-  (forall v theta w,
-   Fpow_emp sigma (S i) v theta w -> F sigma Y v theta w).
+  env_leq (Fpow_emp sigma i) Y ->
+  env_leq (Fpow_emp sigma (S i)) (F sigma Y).
 Proof.
   intros sigma i Y.
-  intros H0 v.
+  intros H0.
   unfold Fpow_emp.
   unfold Fpow.
-  unfold F at 1.
-  unfold F at 2.
-  induction (sigma v);
-  intros theta w H.
-  (* H: (w, theta |= Fpow sigma i, sigma v) *)
-  + (* sigma v = var v0 -> ... *)
-  apply models_var.
-  apply H0.
-  now inversion H.
-  + (* sigma v = l1 .\/ l2 -> ... *)
-  apply models_OR.
-  inversion H as [| | | | w' th' u p1 p2 Ho| | |].
-  destruct Ho as [Ho | Ho].
-  * left; auto.
-  * right; auto.
-  + (* sigma v = l ./\ l0 -> ... *)
-  inversion H.
-  apply models_AND; auto.
-  + (* sigma v = ↓ r, l -> ... *)
-  inversion H as [| h t th u r' psi H' EQu EQht | | | | | |].
-  apply models_STORE; auto.
-  + (* sigma v = X l -> ... *)
-  inversion H.
-  apply models_X; auto.
-  + (* sigma v = l U l0 -> ... *)
-  inversion H
-  as [| | | | | | w' th' u phi psi' Hu EQu EQw'|].
-  clear w' EQw'.
-  destruct Hu as [w' [Hu' Hu]].
-  apply models_U.
-  exists w'.
-  split; auto.
-  + (* sigma v = φ l -> ... *)
-  inversion H.
-  now apply models_PHI.
-  + (* sigma v = G l -> ... *)
-  inversion H.
-  now apply models_G.
+  now apply F_is_monotonic.
 Qed.
 
 Theorem least_fixpoint_of_F :
   forall (sigma : eqn_sys) (Y : Env),
   F sigma Y = Y ->
   forall i,
-  forall v theta w,
-  Fpow_emp sigma i v theta w -> Y v theta w.
+  env_leq (Fpow_emp sigma i) Y.
 Proof.
   intros sigma Y HY i.
   induction i;
   intros v theta w H.
   - now unfold Fpow_emp in H.
   - rewrite<- HY.
-  now apply (F_is_monotonic_2 sigma i Y IHi).
+  now apply (Fpow_is_monotonic_2 sigma i Y IHi).
 Qed.
+
+End Monotonicity.
